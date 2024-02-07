0d45bb451f7ecdb3b0b542e5b94dbc8967830371
Edison Chung <edchung482@gmail.com>
2024-02-07 14:45:49 -0800

    added firebase config file

---
+++ src/config/firebase.config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDAsHSsWozr0_cmW8whKmOHUmzxcuGYmVU",
+  authDomain: "helping-hands-b2698.firebaseapp.com",
+  projectId: "helping-hands-b2698",
+  storageBucket: "helping-hands-b2698.appspot.com",
+  messagingSenderId: "31366880727",
+  appId: "1:31366880727:web:bce6d2786bc764dbe33f92",
+  measurementId: "G-XLVXR6ZMB5"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
