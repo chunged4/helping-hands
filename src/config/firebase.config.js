997e2602328b7efe644f2dd1c4e6ae3a78c7e472
Edison Chung <edchung482@gmail.com>
2024-02-12 14:40:52 -0800

    base login form ui set

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,8 +11,10 @@ const firebaseConfig = {
   storageBucket: "helping-hands-b2698.appspot.com",
   messagingSenderId: "31366880727",
   appId: "1:31366880727:web:bce6d2786bc764dbe33f92",
-  measurementId: "G-XLVXR6ZMB5"
+  measurementId: "G-XLVXR6ZMB5",
 };
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
